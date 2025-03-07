268b003e2b0562b742935425b7f15af8267710ee
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-03-07 16:00:35 +0530

    today

---
+++ pages/contacts/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <SectionsContactsSection1 />
+        <SectionsContactsSection2 />
+    </div>
+</template>
+<script setup>
+    useHead({
+        bodyAttrs: {
+            class: "navbar-light"
+        },
+    })
+</script>
---
+++ pages/faqs/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <SectionsFaqsSection1 />
+    </div>
+</template>
+<script setup>
+    useHead({
+        bodyAttrs: {
+            class: "navbar-light"
+        },
+    })
+</script>
